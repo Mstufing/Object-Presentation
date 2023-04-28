c26c78ada355d04c70479803836d27aa830aa301
Mstufing <mstufingms11@gmail.com>
2023-04-28 05:30:52 +0300

    added object methods like entries, keys and values

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+// ? entries()
+ 
+// let myObject = {
+//     firstName: "Mustafa",
+//     lastName: "Mahmudov",
+//     age: 23,
+//     job: "Software Developer",
+// }
+
+// console.log(Object.entries(myObject));
+
+// ? keys()
+
+// let myObject = {
+//     firstName: "Mustafa",
+//     lastName: "Mahmudov",
+//     age: 23,
+//     job: "Software Developer",
+// }
+
+// console.log(Object.keys(myObject));
+
+// ? values()
+
+let myObject = {
+    firstName: "Mustafa",
+    lastName: "Mahmudov",
+    age: 23,
+    job: "Software Developer",
+}
+
+console.log(Object.values(myObject));
